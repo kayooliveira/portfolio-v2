127f3cc6fced7beeba6014297a28f36b7dfa8124
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-07-16 00:20:43 -0300

    feat(Organizing components in the App component): :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
+import { About } from './components/About'
+import { Header } from './components/Header'
+import { Profile } from './components/Profile'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <Header />
+      <div className="mx-auto mt-40 box-border flex max-w-[1100px] flex-col items-start justify-center gap-8 px-2 lg:flex-row">
+        <Profile />
+        <About />
+      </div>
+    </>
+  )
 }
