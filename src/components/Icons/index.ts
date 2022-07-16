3db33086b087fb76e606e5429abd300b398d5c11
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-07-16 00:17:44 -0300

    feat(exporting all icons from index.ts in components/Icons): :sparkles:

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,27 @@
+import { AWS } from './AWS'
+import { CSS } from './CSS'
+import { GraphQL } from './GraphQL'
+import { HTML } from './HTML'
+import { Javascript } from './Javascript'
+import { Laravel } from './Laravel'
+import { Linux } from './Linux'
+import { MYSQL } from './MYSQL'
+import { Node } from './Node'
+import { Php } from './Php'
+import { ReactJS } from './ReactJS'
+import { Typescript } from './Typescript'
+
+export {
+  AWS,
+  CSS,
+  GraphQL,
+  HTML,
+  Javascript,
+  Laravel,
+  Linux,
+  MYSQL,
+  Node,
+  Php,
+  ReactJS,
+  Typescript
+}
